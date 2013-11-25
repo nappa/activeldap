d0160c6540089c2a02a8341ab48e90d48da74d56
Ryosuke Yamazaki <ryosukey@iij.ad.jp>
2013-11-25 16:54:45 +0900

    set timeout to JNDI.

--- lib/active_ldap/adapter/jndi.rb
+++ lib/active_ldap/adapter/jndi.rb
@@ -23,7 +23,7 @@ module ActiveLdap
           uri = construct_uri(host, port, method == :ssl)
           with_start_tls = method == :start_tls
           info = {:uri => uri, :with_start_tls => with_start_tls}
-          [log("connect", info) {JndiConnection.new(host, port, method)},
+          [log("connect", info) {JndiConnection.new(host, port, method, @timeout)},
            uri, with_start_tls]
         end
       end
@@ -93,8 +93,9 @@ module ActiveLdap
       def execute(method, info=nil, *args, &block)
         name = (info || {}).delete(:name) || method
         log(name, info) {@connection.send(method, *args, &block)}
-      rescue JndiConnection::CommunicationException, JndiConnection::ServiceUnavailableException, => e
+      rescue JndiConnection::CommunicationException, JndiConnection::ServiceUnavailableException => e
         raise ActiveLdap::ConnectionError.new(e.getMessage())
+
       rescue JndiConnection::NamingException
         if /\[LDAP: error code (\d+) - ([^\]]+)\]/ =~ $!.to_s
           message = $2
